40d84d070837671c3ac5571ac3c0da7bd599a5bc
cvasquez1425 <cvasquez1425@hotmail.com>
2015-03-02 13:06:38 -0500

    COMMIT - SubQuery

--- CodeSnippet/LINQ Queries/Class1.cs
+++ CodeSnippet/LINQ Queries/Class1.cs
@@ -21,10 +21,29 @@ namespace LINQ_Queries
                 orderby n.Length                // Sort elements
                 select n.ToUpper();             // Translate each element (projection)
 
-            foreach (string name in query) 
-            {
-                Console.WriteLine(name);
-            }
+            //foreach (string name in query) 
+            //{
+            //    Console.WriteLine(name);
+            //}
+            //Console.ReadKey();
+
+            //SubQuery
+            IEnumerable<string> outerQuery = names
+                            .Where(n => n.Length == names.OrderBy(n2 => n2.Length)
+                                                        .Select(n2 => n2.Length).First());
+            // We can avoid this inefficiency by running the subquery separately ( so that it's no longer a subquery)
+            int shortest = names.Min(n => n.Length);
+
+            IEnumerable<string> query2 = from n in names
+                                        where n.Length == shortest
+                                        select n;
+
+            // Composition Strategies - Progressive Query Building
+            IEnumerable<string> query3 = names
+                .Select(n => Regex.Replace(n, "[aeiou]", ""))
+                .Where(n => n.Length > 2)
+                .OrderBy(n => n);
+
             Console.ReadKey();
         }
     }
