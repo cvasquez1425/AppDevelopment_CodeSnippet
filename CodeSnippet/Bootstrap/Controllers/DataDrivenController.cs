44304a6ffadd60ccd01c61d2d08c45d3c32934b3
cvasquez1425 <cvasquez1425@hotmail.com>
2015-03-06 16:28:31 -0500

    COMMIT - DataDriven01.cshtml Changes

---
+++ CodeSnippet/Bootstrap/Controllers/DataDrivenController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using LINQ.Entities;
+
+namespace Bootstrap.Controllers
+{
+    public class DataDrivenController : Controller
+    {
+        // Data-Driven Horizontal Menu
+        public ActionResult DataDriven01()
+        {
+            PDSAMenuItemManager1 mgr = new PDSAMenuItemManager1();
+
+            mgr.Load();
+            return View(mgr);
+        }
+
+        // GET: DataDriven
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
