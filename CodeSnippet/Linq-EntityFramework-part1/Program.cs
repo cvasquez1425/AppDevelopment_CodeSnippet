13af1440ade62e53614087ff94312619082cea49
cvasquez1425 <cvasquez1425@hotmail.com>
2015-06-17 09:35:29 -0400

    Commit

--- CodeSnippet/Linq-EntityFramework-part1/Program.cs
+++ CodeSnippet/Linq-EntityFramework-part1/Program.cs
@@ -17,9 +17,16 @@ namespace Linq_EntityFramework_part1
                     where m.Release_Date > new DateTime(2000, 1, 1)
                     select m;
 
+
+                //ctx.Log = Response.Output;
+
                 foreach (var movie in query.Take(15))
                 {
-                    Console.WriteLine(movie.Title, movie.Reviews, movie.Release_Date);
+                    Console.WriteLine(movie.Title);
+                    foreach (Review r in movie.Reviews)
+                    {
+                        Console.WriteLine("\t{0}:{1}", r.Reviewer, r.Rating); 
+                    }
                 }
 
                 Console.WriteLine("Press any key to exit....");
