08e31b4e7fdd24fe379cefd3b182ca3343c39b33
cvasquez1425 <cvasquez1425@hotmail.com>
2015-07-09 15:44:05 -0400

    Commit Logging and Migration of Schema Changes to EF

--- CodeSnippet/Books.Web/DataContexts/BooksDb.cs
+++ CodeSnippet/Books.Web/DataContexts/BooksDb.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Data.Entity;
+using System.Diagnostics;
 using System.Linq;
 using System.Web;
 
@@ -12,6 +13,17 @@ namespace Books.Web.DataContexts
         public BooksDb()
             :base("DefaultConnection")
         {
+            // Logging. I do that in the constructor, so every instance goes thru this
+            Database.Log = sql => Debug.Write(sql);  //it'll appear in the VS Debug output when I am running with the debugger in the system.Diagnostics Namespace
+        }
+
+        /// <summary>
+        /// I can override OnModelCreating and I can tell the model builder that everything on this DbContext has a DEFAULT Schema which is no DBO, it'll be the library schema
+        /// </summary>
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.HasDefaultSchema("library");
+            base.OnModelCreating(modelBuilder);
         }
 
         public DbSet<Book> Books { get; set; }
--- CodeSnippet/Books.Web/DataContexts/IdentityDb.cs
+++ CodeSnippet/Books.Web/DataContexts/IdentityDb.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNet.Identity.EntityFramework;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 
@@ -12,6 +13,14 @@ namespace Books.Web.DataContexts
         public IdentityDb()
             : base("DefaultConnection")
         {
+        }
+        /// <summary>
+        /// I can override OnModelCreating and I can tell the model builder that everything on this DbContext has a DEFAULT Schema which is no DBO, it'll be the library schema
+        /// </summary>
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.HasDefaultSchema("identity");
+            base.OnModelCreating(modelBuilder);
         }
     }
 }
