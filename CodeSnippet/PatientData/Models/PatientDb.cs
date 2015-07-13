3555bbd3a5232d5642b639dac25ddfce4ef45b58
cvasquez1425 <cvasquez1425@hotmail.com>
2015-07-13 16:00:48 -0400

    Commit MongoDB - ThinkPad

---
+++ CodeSnippet/PatientData/Models/PatientDb.cs
@@ -0,0 +1,20 @@
+﻿using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MongoDB.Bson;
+
+namespace PatientData.Models
+{
+    public static class PatientDb
+    {
+        public static MongoCollection<PatientData> Open()
+        {
+            var client = new MongoClient("mongodb://localhost");
+            var server = client.GetServer();
+            var db = server.GetDatabase("PatientDb");
+            return db.GetCollection<PatientData>("Patients");
+        }
+    }
+}
