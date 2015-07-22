18ab8c39840a9872c50bd8e8b317f24703c9f0c5
cvasquez1425 <cvasquez1425@hotmail.com>
2015-07-22 16:03:27 -0400

    Commit CORS PluralSight ThinkPad

--- CodeSnippet/PatientData/App_Start/WebApiConfig.cs
+++ CodeSnippet/PatientData/App_Start/WebApiConfig.cs
@@ -5,6 +5,7 @@ using System.Net.Http;
 using System.Web.Http;
 using Microsoft.Owin.Security.OAuth;
 using Newtonsoft.Json.Serialization;
+using System.Web.Http.Cors;
 
 namespace PatientData
 {
@@ -16,6 +17,10 @@ namespace PatientData
             // Configure Web API to use only bearer token authentication.
             config.SuppressDefaultHostAuthentication();
             config.Filters.Add(new HostAuthenticationFilter(OAuthDefaults.AuthenticationType));
+
+            // Enable CORS which is very useful in this world where Single-Page Applications often have to call API on different servers, you have mashups, grabbing data from everywhere
+            var cors = new EnableCorsAttribute("*", "*", "GET");
+            config.EnableCors();
 
             // Web API routes
             // Notice that comes before the default route and that gives any attribute routes that 
--- CodeSnippet/PatientData/Controllers/PacientesController.cs
+++ CodeSnippet/PatientData/Controllers/PacientesController.cs
@@ -6,9 +6,11 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using System.Web.Http.Cors;
 
 namespace PatientData.Controllers
 {
+    [EnableCors("*", "*", "GET")]
     public class PacientesController : ApiController
     {
 
